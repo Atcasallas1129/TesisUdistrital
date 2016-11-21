3ce3f93a5e6403e11c7e98dbac9cbf0c1830d0f8
Alvaro Tovar Casallas <alvaro_tovarc89@hotmail.com>
2016-11-21 01:19:31 -0500

    correccion del modelo de datos remoto

--- TesisUdistrital/RemoteModel.Context.cs
+++ TesisUdistrital/RemoteModel.Context.cs
@@ -32,7 +32,6 @@ namespace TesisUdistrital
         public virtual DbSet<hechoVictimizante> hechoVictimizante { get; set; }
         public virtual DbSet<parentesco> parentesco { get; set; }
         public virtual DbSet<soporte> soporte { get; set; }
-        public virtual DbSet<soporteXRadicado> soporteXRadicado { get; set; }
         public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
         public virtual DbSet<tipoDocumento> tipoDocumento { get; set; }
         public virtual DbSet<usuario> usuario { get; set; }
@@ -41,6 +40,7 @@ namespace TesisUdistrital
         public virtual DbSet<procesoDocumentacion> procesoDocumentacion { get; set; }
         public virtual DbSet<vw_consultaCasoDocumentacion> vw_consultaCasoDocumentacion { get; set; }
         public virtual DbSet<vw_informacionPersona> vw_informacionPersona { get; set; }
+        public virtual DbSet<soporteXRadicado> soporteXRadicado { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
